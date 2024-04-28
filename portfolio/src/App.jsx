9babcf21f924b7b238d8b1bee1127acb4f0eb05e
VinayJangotra <109384165+VinayJangotra@users.noreply.github.com>
2024-04-28 18:11:02 +0530

    Timepass

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import styles from"./App.module.css";
+import { Hero } from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
   return (
     <div className={styles.App}>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
